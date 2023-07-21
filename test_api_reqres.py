d89c2388556e92deb9f1e7c1ed62fbc295381a80
Tehir137 <texsex137@gmail.com>
2023-07-21 22:48:57 +0300

    Доделал

--- test_api_reqres.py
+++ test_api_reqres.py
@@ -32,4 +32,74 @@ def test_dict_user ():
         logging.info(r.json())
         assert r.status_code == 200
         assert len(r.json()['data']) == dict_element
+        assert r.json()['data']['id'] == 1
+
+def test_update_user():
+    with allure.step('Обновление данных УЗ'):
+        id_user = '/7'
+        first_name = 'Slava'
+        email = 'slavaslava@mail.ru'
+        last_name = 'Beza'
+        r = requests.get(f'{base_url}{path_users}{id_user}',json={'first_name': first_name, 'last_name': last_name, 'email': email})
+        logging.info(r.json())
+        assert r.status_code == 200
+
+def test_delete_user():
+    with allure.step('Удаление УЗ'):
+        id_user = '/7'
+        r = requests.delete(f'{base_url}{path_users}{id_user}')
+        assert r.status_code == 204
+        assert r.text == ""
+
+def test_reg_successful():
+    with allure.step('Заведение УЗ успешное'):
+        email = 'eve.holt@reqres.in'
+        passw ='qwerty'
+        iden = 4
+        token = 'QpwL5tke4Pnpja7X4'
+        r = requests.post(f'{base_url}{path_register}', json={'email': email, 'password': passw})
+        logging.info(r.json())
+        assert r.status_code == 200
+        assert r.json()['id'] == iden
+        assert r.json()['token'] == token
+
+def test_wrong_reg():
+    with allure.step("Ошибка регистрации"):
+        email = 'sydney@fife'
+        error_message = 'Missing password'
+        r = requests.post(f'{base_url}{path_register}', json={'email': email})
+        logging.info(r.json())
+        assert r.status_code == 400
+        assert r.status_code == requests.codes.bad_request
+        assert r.json()['error'] == error_message
+
+def test_login_successful():
+    with allure.step('Авторизация'):
+        email = 'eve.holt@reqres.in'
+        passw = 'qwerty'
+        token = 'QpwL5tke4Pnpja7X4'
+        r = requests.post(f'{base_url}{path_login}', json={'email': email, 'password': passw})
+        logging.info(r.json())
+        assert r.status_code == requests.codes.ok
+        assert r.json()['token'] == token
+
+def test_wrong_login():
+    with allure.step("Ошибка авторизации"):
+        email = 'peter@klaven'
+        error_message = 'Missing password'
+        r = requests.post(f'{base_url}{path_register}', json={'email': email})
+        logging.info(r.json())
+        assert r.status_code == 400
+        assert r.status_code == requests.codes.bad_request
+        assert r.json()['error'] == error_message
+
+def test_delayed_response():
+    with allure.step('Delyaed presponse'):
+        delay = 3
+        r = requests.get(f'{base_url}{path_users }', params={'delay': delay})
+        logging.info(r.json())
+        assert r.status_code == 200
+        assert r.json()['total'] == 12
+        assert r.json()["total_pages"] == 2
+
 
